8dd9385cafebc2b7d1abcc4ba7ce93dc1f65439e
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2025-01-02 13:58:50 +0200

    feat: enable content-security-policy prop

--- src/main/java/com/iqkv/boot/security/SecurityProperties.java
+++ src/main/java/com/iqkv/boot/security/SecurityProperties.java
@@ -6,8 +6,17 @@ import org.springframework.web.cors.CorsConfiguration;
 @ConfigurationProperties(prefix = "iqkv.security", ignoreUnknownFields = false)
 public class SecurityProperties {
 
+  private String contentSecurityPolicy = "default-src 'self'; frame-src 'self' data:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://storage.googleapis.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:";
   private final CorsConfiguration cors = new CorsConfiguration();
   private final AuthenticationProperties authentication = new AuthenticationProperties();
+
+  public String getContentSecurityPolicy() {
+    return contentSecurityPolicy;
+  }
+
+  public void setContentSecurityPolicy(String contentSecurityPolicy) {
+    this.contentSecurityPolicy = contentSecurityPolicy;
+  }
 
   public CorsConfiguration getCors() {
     return cors;
